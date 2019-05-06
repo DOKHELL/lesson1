1196046ca599fc153c1e5ee00d14b4b9a6b34ec4
DOKHELL <48280426+DOKHELL@users.noreply.github.com>
2019-05-06 15:44:40 +0300

    fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,6 @@ $(function () {
         dots: true,
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-        infinite: false,
     });
 
 });
