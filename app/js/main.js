8d5620d62960dc056a16cebc214bb08006266479
DOKHELL <48280426+DOKHELL@users.noreply.github.com>
2019-05-08 18:52:51 +0300

    fixsed

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,52 @@
 $(function () {
-    $('.slider-items').slick({
-        arrows: true,
-        dots: true,
-        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    });
+  $('.slider-items').slick({
+    arrows: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+
+  });
 
 });
-$("#search-fancy").on('click', function () {
-    $.fancybox.open('<div class="message"><h2>Hello!</h2><p>You are awesome!</p></div>');
-});
-$('[data-fancybox="preview"]').fancybox({
-    thumbs : {
-      autoStart : true
+$(function initHolderBg() {
+  "use strict";
+  $('.slider-items .slider-item .slider-item__img').each(function () {
+    var imgHolder = $(this),
+      img = imgHolder.find('img'),
+      imgSrc = img.attr('src');
+    if (!!imgHolder.length && !!img.length && !!imgSrc) {
+      imgHolder.css({
+        backgroundImage: 'url(' + imgSrc + ')',
+        backgroundSize: 'cover',
+        backgroundPosition: '50% 50%',
+        backgroundRepeat: 'no-repeat',
+        height: '100%'
+      });
+      img.remove();
     }
   });
-  $(function(){
-    var link = $('.m-menu-link');
-    var close = $('.close-menu');
-    var menu = $('.m-menu');
+})
 
-    link.on('click', function(event){
-            event.preventDefault();
-            menu.toggleClass('m-menu__active');
-    });
-    close.on('click', function(event){
-            event.preventDefault();
-            menu.toggleClass('m-menu__active');
-    });
+
+$("#search-fancy").on('click', function () {
+  $.fancybox.open('<div class="message"><h2>Hello!</h2><p>You are awesome!</p></div>');
+});
+$('[data-fancybox="preview"]').fancybox({
+  thumbs: {
+    autoStart: true
+  }
+});
+$(function () {
+  var link = $('.m-menu-link');
+  var close = $('.close-menu');
+  var menu = $('.m-menu');
+
+  link.on('click', function (event) {
+    event.preventDefault();
+    menu.toggleClass('m-menu__active');
+  });
+  close.on('click', function (event) {
+    event.preventDefault();
+    menu.toggleClass('m-menu__active');
+  });
 });
