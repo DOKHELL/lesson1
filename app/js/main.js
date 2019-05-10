2f7ade62965f96f45c11fb994cee57e2df38e7fb
DOKHELL <48280426+DOKHELL@users.noreply.github.com>
2019-05-10 16:20:27 +0300

    hope end...

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,12 @@
-$(function () {
+function slider() {
   $('.slider-items').slick({
     arrows: true,
     dots: true,
     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
   });
-});
-
-
-$(function initHolderBg() {
+}
+function initHolderBg() {
   "use strict";
   $('.slider-items .slider-item .slider-item__img').each(function () {
     var imgHolder = $(this),
@@ -25,46 +23,54 @@ $(function initHolderBg() {
       img.remove();
     }
   });
-})
-
-
-$("#search-fancy").on('click', function () {
-  $.fancybox.open('<div class="message"><h2>Hello!</h2><p>You are awesome!</p></div>');
-});
-
-$('[data-fancybox="preview"]').fancybox({
-  thumbs: {
-    autoStart: true
-  }
-});
-
-
-$(function () {
-  $('.services__matchheight').matchHeight();
-});
-
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
 }
-window.onclick = function(event) {
-if (!event.target.matches('.dropbtn')) {
-
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
+function searchfancy() {
+  $("#search-fancy").on('click', function () {
+    $.fancybox.open('<div class="message"><h2>Hello!</h2><p>You are awesome!</p></div>');
+  });
+}
+function galeryfancy() {
+  $('[data-fancybox="preview"]').fancybox({
+    thumbs: {
+      autoStart: true
+    }
+  });
+}
+function matchheight() {
+  $('.services__matchheight').matchHeight();
+}
+function dropdowns() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+  
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
   }
 }
+function burgermenu(){
+  $("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked"); 
+  });
 }
 
 
-$("#navToggle").click(function() {
-  $(this).toggleClass("active");
-  $(".overlay").toggleClass("open");
-  // this line ▼ prevents content scroll-behind
-  $("body").toggleClass("locked"); 
-});
+
+
+//Runs
+$( document ).ready( slider );
+$( document ).ready( initHolderBg );
+$( document ).ready( searchfancy );
+$( document ).ready( galeryfancy );
+$( document ).ready( matchheight );
+$( document ).ready( burgermenu );
